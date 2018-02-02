109b2e7e70afd7b41f86030e1041b79cafca920c
segabo1718daw2 <segabo1718daw2@gmail.com>
2018-02-02 16:58:16 +0100

    Prova d'IC amb Jenkins

---
+++ src/jenkins/Jenkins.java
@@ -0,0 +1,32 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package jenkins;
+/**
+ *
+ * @author segabo1718daw2
+ */
+import java.io.*;
+public class Jenkins {
+    /**
+     * @param args the command line arguments
+     * @throws java.io.IOException
+     */
+    public static void main(String[] args) throws IOException {
+        File f = new File("index.html");
+        try (BufferedWriter bw = new BufferedWriter(new FileWriter(f))) {
+            bw.write("<html>");bw.newLine();
+            bw.write("  <head>");bw.newLine();
+            bw.write("    <title>");bw.newLine();
+            bw.write("      Treballant amb l'IC Jenkins");bw.newLine();
+            bw.write("    </title>");bw.newLine();
+            bw.write("  </head>");bw.newLine();
+            bw.write("  <body>");bw.newLine();
+            bw.write("    Projecte per provar l'IC Jenkins");bw.newLine();
+            bw.write("  </body>");bw.newLine();
+            bw.write("</html>");bw.newLine();
+            bw.close();
+        }
+    }
+}
